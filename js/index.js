01919108aa6ccd06f46f78e276626f495b163c07
anthonyg876 <anthonygravier2@gmail.com>
2022-07-31 23:15:48 -0400

    Fixed webpage to now take in a json for the output of the fetch

--- js/index.js
+++ js/index.js
@@ -1,10 +1,31 @@
 
+async function getJson(url) {
+    const response = await fetch(url);
+    return response.json();
+}
+
 async function getStocks() {
     const name = document.getElementById('nameOfStock');
-    const year = document.getElementById('Year');
+    
+    const heapSort = document.getElementById("HeapSort");
+    const TimSort = document.getElementById("TimSort");
+    let outPut;
+    let algorithm;
+
+    console.log(TimSort.checked);
+    console.log(heapSort.value);
+    
 
     document.getElementById("outPutStock").value = document.getElementById('nameOfStock').value;
 
-    fetch('http://localhost:8080/stocks/' + name.value).then(response => response.json())
-    .then(data => console.log(data));
+    if (document.getElementById("HeapSort").checked) {
+        algorithm = heapSort;
+    }
+    else {
+        algorithm = TimSort;
+    }
+
+    const jsonData = await getJson('http://localhost:8080/stocks/' + name.value + "/" + algorithm.value);
+
+    console.log({ jsonData });
 }   
